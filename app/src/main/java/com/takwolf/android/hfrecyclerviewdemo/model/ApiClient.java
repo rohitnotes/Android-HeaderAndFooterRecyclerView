6348d31bf881f791d52d34b1fb06eb34911e638b
TakWolf <takwolf@foxmail.com>
2017-05-30 18:03:24 +0800

    调整数据

--- app/src/main/java/com/takwolf/android/hfrecyclerviewdemo/model/ApiClient.java
+++ app/src/main/java/com/takwolf/android/hfrecyclerviewdemo/model/ApiClient.java
@@ -31,7 +31,30 @@ public final class ApiClient {
             new Illust(UUID.randomUUID().toString(), "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=120376652,2243603085&fm=23&gp=0.jpg"),
             new Illust(UUID.randomUUID().toString(), "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1421089861,3387353081&fm=23&gp=0.jpg"),
             new Illust(UUID.randomUUID().toString(), "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2974606076,876377175&fm=23&gp=0.jpg"),
-            new Illust(UUID.randomUUID().toString(), "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=917773156,1903857270&fm=23&gp=0.jpg")
+            new Illust(UUID.randomUUID().toString(), "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=917773156,1903857270&fm=23&gp=0.jpg"),
+            new Illust(UUID.randomUUID().toString(), "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2881199508,3573796903&fm=23&gp=0.jpg"),
+            new Illust(UUID.randomUUID().toString(), "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3428158096,150527777&fm=23&gp=0.jpg"),
+            new Illust(UUID.randomUUID().toString(), "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2050503217,1842587783&fm=23&gp=0.jpg"),
+            new Illust(UUID.randomUUID().toString(), "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2121478003,505202122&fm=23&gp=0.jpg"),
+            new Illust(UUID.randomUUID().toString(), "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1321013049,445640085&fm=23&gp=0.jpg"),
+            new Illust(UUID.randomUUID().toString(), "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=230697610,2029523791&fm=23&gp=0.jpg"),
+            new Illust(UUID.randomUUID().toString(), "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=20179292,4203438564&fm=23&gp=0.jpg"),
+            new Illust(UUID.randomUUID().toString(), "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=143698042,2353673474&fm=23&gp=0.jpg"),
+            new Illust(UUID.randomUUID().toString(), "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2453962847,3087001482&fm=23&gp=0.jpg"),
+            new Illust(UUID.randomUUID().toString(), "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=2429798492,356704922&fm=23&gp=0.jpg"),
+            new Illust(UUID.randomUUID().toString(), "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3236426775,338253480&fm=23&gp=0.jpg"),
+            new Illust(UUID.randomUUID().toString(), "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1349022575,1961474003&fm=23&gp=0.jpg"),
+            new Illust(UUID.randomUUID().toString(), "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=84450610,2184556309&fm=23&gp=0.jpg"),
+            new Illust(UUID.randomUUID().toString(), "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=719265366,2637598969&fm=23&gp=0.jpg"),
+            new Illust(UUID.randomUUID().toString(), "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2445637883,3738647417&fm=23&gp=0.jpg"),
+            new Illust(UUID.randomUUID().toString(), "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2382996667,1154920146&fm=23&gp=0.jpg"),
+            new Illust(UUID.randomUUID().toString(), "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2813433988,1349404678&fm=23&gp=0.jpg"),
+            new Illust(UUID.randomUUID().toString(), "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1665081333,2173629569&fm=23&gp=0.jpg"),
+            new Illust(UUID.randomUUID().toString(), "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=4139348679,3420835154&fm=23&gp=0.jpg"),
+            new Illust(UUID.randomUUID().toString(), "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2214572700,297715454&fm=23&gp=0.jpg"),
+            new Illust(UUID.randomUUID().toString(), "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1708456250,4089965159&fm=23&gp=0.jpg"),
+            new Illust(UUID.randomUUID().toString(), "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1819319106,2665274594&fm=23&gp=0.jpg"),
+            new Illust(UUID.randomUUID().toString(), "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1097612239,3740592325&fm=23&gp=0.jpg")
     };
 
     private static final Random random = new Random();
