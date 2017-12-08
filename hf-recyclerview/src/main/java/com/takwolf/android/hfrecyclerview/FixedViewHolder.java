2014c04efaf954cbbb9678b62a6ee24066478027
TakWolf <takwolf@foxmail.com>
2017-12-08 11:52:35 +0800

    FixedView type header 和 footer 类型定义改为 Integer.MIN_VALUE 以避免碰撞冲突

--- hf-recyclerview/src/main/java/com/takwolf/android/hfrecyclerview/FixedViewHolder.java
+++ hf-recyclerview/src/main/java/com/takwolf/android/hfrecyclerview/FixedViewHolder.java
@@ -5,8 +5,8 @@ import android.view.View;
 
 public final class FixedViewHolder extends RecyclerView.ViewHolder {
 
-    public static final int VIEW_TYPE_HEADER = -1;
-    public static final int VIEW_TYPE_FOOTER = -2;
+    public static final int VIEW_TYPE_HEADER = Integer.MIN_VALUE;
+    public static final int VIEW_TYPE_FOOTER = Integer.MIN_VALUE + 1;
 
     FixedViewHolder(View itemView) {
         super(itemView);
